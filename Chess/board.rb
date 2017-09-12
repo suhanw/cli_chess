3aad4db79c23b52e23a41fd6acfdc316199efa8c
Suhan Wijaya <suhanw@gmail.com>
2017-09-12 18:02:01 -0400

    Update chess

--- Chess/board.rb
+++ Chess/board.rb
@@ -70,12 +70,77 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    raise ChessError.new("No piece to move!")if self[start_pos].nil?
-    # debugger
-    raise ChessError.new("Piece exists at end_pos")unless self[end_pos].nil?
+    raise ChessError.new("No piece to move!") if self[start_pos].is_a?(NullPiece)
+    # raise ChessError.new("Piece exists at end_pos")unless self[end_pos].nil?
+    unless self[start_pos].valid_moves.include?(end_pos)
+      raise ChessError.new("Not a valid move!")
+    end
+
+    self[end_pos], self[start_pos] = self[start_pos], NullPiece.instance
+    self[end_pos].pos = end_pos
+  end
+
+  def move_piece!(start_pos, end_pos)
+    self[end_pos], self[start_pos] = self[start_pos], NullPiece.instance
+    self[end_pos].pos = end_pos
   end
 
   def in_bounds(pos)
     pos.all? {|coordinate| coordinate.between?(0, 7) }
   end
+
+  def in_check?(color)
+    king_pos = []
+    opponent_moves = []
+    self.board.each do |row|
+      row.each do |piece|
+        # (1) finding the position of the King on the board then
+        if piece.class == King && piece.color == color
+          king_pos = piece.pos
+        # (2) seeing if any of the opposing pieces can move to that position.
+        elsif piece.color != color
+          opponent_moves += piece.moves
+        end
+      end
+    end
+
+    # return true if opponent moves include king position
+    opponent_moves.include?(king_pos)
+  end
+
+  def checkmate?(color)
+    in_check = self.in_check?(color)
+    no_valid_moves = true
+    # debugger
+     @board.each do |row|
+      row.each do |piece|
+        # debugger
+        if piece.color == color && !piece.valid_moves.empty?
+          no_valid_moves = false
+        end
+        # debugger
+      end
+    end
+    in_check && no_valid_moves
+  end
+
+  def dup
+    board_dup = Board.new
+    (0..7).to_a.each do |row_i|
+      (0..7).to_a.each do |col_i|
+        if self[[row_i, col_i]].is_a?(NullPiece)
+          board_dup[[row_i, col_i]] = NullPiece.instance
+        else
+          # board_dup[[row_i, col_i]] = self[[row_i, col_i]].dup
+          piece = self[[row_i, col_i]]
+          board_dup[[row_i, col_i]] = piece.class.new(piece.color, piece.pos.dup, board_dup)
+          # board_dup[[row_i, col_i]].board = board_dup
+        end
+      end
+    end
+    return board_dup
+  end
+
+  protected
+  attr_reader :board
 end
--- Chess/piece.rb
+++ Chess/piece.rb
@@ -3,15 +3,32 @@ require_relative "modules"
 require_relative "board"
 
 class Piece
-  attr_reader :symbol, :color, :move_dirs, :pos, :board
+  attr_reader :symbol, :color, :move_dirs
+  attr_accessor :board, :pos
+
   def initialize(color, pos, board)
     @color = color
     @pos = pos
     @board = board
   end
+
+  def valid_moves
+    # 1. filter out moves that will cause king to be in check
+    # move_into_check?
+    # will call move_into_check? so we CANNOT call move_piece
+    all_moves = self.moves
+    all_moves.reject { |move| self.move_into_check?(move) }
+  end
+
+  def move_into_check?(end_pos)
+    board_dup = self.board.dup
+    board_dup.move_piece!(self.pos, end_pos)
+    board_dup.in_check?(self.color)
+  end
 end
 
 class King < Piece
+  include SteppingPiece
 
   def initialize(color, pos, board)
     super(color, pos, board)
@@ -20,10 +37,22 @@ class King < Piece
     else
       @symbol = "\u2654"
     end
+    @move_dirs = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+      [1, 1],
+      [-1, -1],
+      [1, -1],
+      [-1, 1]
+    ]
   end
 end
 
 class Knight < Piece
+  include SteppingPiece
+
   def initialize(color, pos, board)
     super(color, pos, board)
     if color == "black"
@@ -31,6 +60,16 @@ class Knight < Piece
     else
       @symbol = "\u2658"
     end
+    @move_dirs = [
+      [-2, -1],
+      [-2,  1],
+      [-1, -2],
+      [-1,  2],
+      [ 1, -2],
+      [ 1,  2],
+      [ 2, -1],
+      [ 2,  1]
+    ]
   end
 end
 
@@ -41,6 +80,72 @@ class Pawn < Piece
       @symbol = "\u265f"
     else
       @symbol = "\u2659"
+    end
+
+  end
+
+  def moves
+    moves_array = []
+
+    moves_array += side_attacks
+    forward_steps.each do |move|
+      new_pos = [self.pos[0] + move[0], self.pos[1] + move[1]]
+      if self.board.in_bounds(new_pos) && self.board[new_pos].is_a?(NullPiece)
+        moves_array.push(new_pos)
+      end
+    end
+
+    moves_array
+  end
+
+  def side_attacks
+    if self.color == 'white'
+      left_diag = [self.pos[0] - 1, self.pos[1] - 1]
+      right_diag = [self.pos[0] - 1, self.pos[1] + 1]
+    else
+      left_diag = [self.pos[0] + 1, self.pos[1] - 1]
+      right_diag = [self.pos[0] + 1, self.pos[1] + 1]
+    end
+
+    moves_array = []
+    if self.board.in_bounds(left_diag)
+      if self.board[left_diag].color != self.color && !self.board[left_diag].color.nil?
+        moves_array.push(left_diag)
+      end
+    end
+    if self.board.in_bounds(right_diag)
+      if self.board[right_diag].color != self.color && !self.board[right_diag].color.nil?
+        moves_array.push(right_diag)
+      end
+    end
+
+    moves_array
+  end
+
+  def forward_steps
+    if self.at_start_row?
+      # 1 step or 2 steps
+      if self.color == 'black'
+        [[1, 0], [2, 0]]
+      else
+        [[-1, 0], [-2, 0]]
+      end
+
+    else
+      # just 1 step
+      if self.color == 'black'
+        [[1, 0]]
+      else
+        [[-1, 0]]
+      end
+    end
+  end
+
+  def at_start_row?
+    if self.color == 'black'
+      self.pos[0] == 1
+    else
+      self.pos[0] == 6
     end
   end
 end
@@ -113,5 +218,10 @@ class NullPiece < Piece
   include Singleton
   def initialize
     @symbol = " "
+  end
+
+  def moves
+    # DUCK TYPING
+    []
   end
 end
