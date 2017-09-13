7255fffcc4ff5d6548a1a15da222b2f1f92d2834
Suhan Wijaya <suhanw@gmail.com>
2017-09-13 08:54:37 -0400

    Add game and player

--- Chess/game.rb
+++ Chess/game.rb
@@ -1,3 +1,49 @@
 require_relative 'board'
 require_relative 'display'
 require_relative 'player'
+
+class Game
+  attr_reader :b, :player1, :player2, :curr_player, :d
+
+  def initialize
+    @b = Board.new
+    @d = Display.new(b)
+    @player1 = HumanPlayer.new('black', @d)
+    @player2 = HumanPlayer.new('white', @d)
+    @curr_player = player1
+  end
+
+  def play
+    puts "Welcome to CHESS!"
+
+    until self.b.checkmate?(@curr_player.color)
+      begin
+        notify_players
+        # @d.render
+        @curr_player.make_move
+        system("clear")
+      rescue ChessError => e
+        puts e.message
+        sleep 0.5
+        system("clear")
+        retry
+      end
+
+      @curr_player = @curr_player == @player1 ? @player2 : @player1
+    end
+
+  end
+
+  def notify_players
+    puts "It's #{@curr_player.color}'s turn."
+  end
+
+  def swap_turn
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+  g = Game.new
+  g.play
+end
--- Chess/player.rb
+++ Chess/player.rb
@@ -1,1 +1,15 @@
 require_relative 'display'
+require_relative 'cursor'
+
+class HumanPlayer
+  attr_reader :color, :display
+
+  def initialize(color, display)
+    @color = color
+    @display = display
+  end
+
+  def make_move
+    @display.render_cursor_move
+  end
+end
